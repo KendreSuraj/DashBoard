ab4597035c9f33475d03b2b894722c6d6183547f
vaibhav-avataar <vaibhav.sharma@avataarskin.com>
2024-01-15 17:26:46 +0530

    user details

---
+++ src/components/common/BookingComponent/UserDetailComponent.jsx
@@ -0,0 +1,52 @@
+// UserDetailsComponent
+
+import React from 'react';
+import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+
+const UserDetailsComponent = ({ data }) => {
+    const keys = Object.keys(data);
+    const middleIndex = Math.ceil(keys.length / 2);
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+                <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
+                    <TableContainer style={{ overflowX: 'auto' }}>
+                        <Table>
+                            <TableBody>
+                                {keys.slice(0, middleIndex).map((key) => (
+                                    <TableRow key={key}>
+                                        <TableCell>
+                                            <strong>{key}:</strong>
+                                        </TableCell>
+                                        <TableCell>{data[key]}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
+                    <TableContainer style={{ overflowX: 'auto' }}>
+                        <Table>
+                            <TableBody>
+                                {keys.slice(middleIndex).map((key) => (
+                                    <TableRow key={key}>
+                                        <TableCell>
+                                            <strong>{key}:</strong>
+                                        </TableCell>
+                                        <TableCell>{data[key]}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default UserDetailsComponent;
