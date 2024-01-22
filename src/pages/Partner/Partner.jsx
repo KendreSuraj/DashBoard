50d14fa69991c4e0dd13b36544f88a11e3b9657b
ShaliniKatiyar <intern.developer@avataarskin.com>
2024-01-22 16:14:25 +0530

    restrict concentrix user

--- src/pages/Partner/Partner.jsx
+++ src/pages/Partner/Partner.jsx
@@ -7,20 +7,23 @@ import LoaderComponent from '../../components/common/LoaderComponent/LoaderCompo
 const Partner = () => {
   const dispatch = useDispatch();
   const partnerList = useSelector((state) => state.partner.partnerList);
+  const stringifiedUser = localStorage.getItem("userData")
+  const userData = stringifiedUser? JSON.parse(stringifiedUser):null
+  const concentrixUser = userData && userData.user && userData.user.concentrixUser? userData.user.concentrixUser:false 
 
   useEffect(() => {
     dispatch(fetchPartner());
   }, [dispatch]);
 
   return (
-    <div>
-      <h3>All Partner List</h3>
-      {partnerList?.length > 0 ? (
-        <TableComponent data={partnerList} hiddenFields={['partner_id']} />
-      ) : (
-        <LoaderComponent />
-      )}
-    </div>
+    concentrixUser? <h1>You do not have access for this</h1>:<div>
+    <h3>All Partner List</h3>
+    {partnerList?.length > 0 ? (
+      <TableComponent data={partnerList} hiddenFields={['partner_id']} />
+    ) : (
+      <LoaderComponent />
+    )}
+  </div>
   );
 };
 
