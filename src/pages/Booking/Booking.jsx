399d9b414272709f2c3240fd7310d9e44d531276
ShaliniKatiyar <intern.developer@avataarskin.com>
2024-02-02 18:53:31 +0530

    start time and end time

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -8,39 +8,34 @@ import LoaderComponent from '../../components/common/LoaderComponent/LoaderCompo
 import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
 
-
 const Booking = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
 
   let bookingList = useSelector((state) => state.booking.bookingList);
-  
-  bookingList=bookingList?.map(data=>{
+
+  bookingList = bookingList?.map((data) => {
     const newDate = moment(data.appointmentAt);
     return {
-      Name: data.name ? data.name: "",
+      Name: data.name ? data.name : '',
       'Phone Number': data.phoneNumber,
-      City: data.addressCity? data.addressCity: "",
-      "Service Name": data.productName? data.productName:"",
-      "Booking Date": newDate.format('YYYY-MM-DD'),
-      "Booking Time": newDate.format('hh:mm A'),
-      Building: data.building?data.building:"",
-      "Formatted Address": data.formattedAddress? data.formattedAddress:"",
-      "Product Price": data.productPrice? data.productPrice:"",
-      "Discount": data.discount? data.discount:"",
-      "Coupon Discount": data.couponDiscount? data.couponDiscount:"",
-      Total: data.total? data.total:"",
-      Count: data.count? data.count:"",
-      "Pin Code": data.addressPostalCode? data.addressPostalCode:"",
-      Status: data.status? data.status: "",
-      map: "https://www.google.com/maps/place/Avataar+Skincare+Technologies/@28.5696137,77.2235768,17z/data=!3m1!4b1!4m6!3m5!1s0xa8a9a9711674473d:0xc2f776fea8e73de8!8m2!3d28.5696137!4d77.2261517!16s%2Fg%2F11t768tgtm?entry=ttu",
-      sessionId: data.sessionId? data.sessionId:null,
-
-    }
-  })
-
-
+      City: data.addressCity ? data.addressCity : '',
+      'Service Name': data.productName ? data.productName : '',
+      'Booking Date': newDate.format('YYYY-MM-DD'),
+      'Booking Time': newDate.format('hh:mm A'),
+      Building: data.building ? data.building : '',
+      'Formatted Address': data.formattedAddress ? data.formattedAddress : '',
+      'Product Price': data.productPrice ? data.productPrice : '',
+      Discount: data.discount ? data.discount : '',
+      'Coupon Discount': data.couponDiscount ? data.couponDiscount : '',
+      Total: data.total ? data.total : '',
+      Count: data.count ? data.count : '',
+      'Pin Code': data.addressPostalCode ? data.addressPostalCode : '',
+      Status: data.status ? data.status : '',
+      sessionId: data.sessionId ? data.sessionId : null,
+      map: data.Map ? data.Map : '',
+    };
+  });
 
   const today = new Date();
   const tomorrow = new Date();
@@ -58,19 +53,23 @@ const Booking = () => {
   };
 
   const handleBookingDetail = (details) => {
-    navigate(`/booking-details/${details.sessionId}`)
-  }
+    navigate(`/booking-details/${details.sessionId}`);
+  };
 
-  const stringifiedUser = localStorage.getItem("userData")
-  const userData = stringifiedUser? JSON.parse(stringifiedUser):null
-  const concentrixUser = userData && userData.user && userData.user.concentrixUser? userData.user.concentrixUser:false 
+  const stringifiedUser = localStorage.getItem('userData');
+  const userData = stringifiedUser ? JSON.parse(stringifiedUser) : null;
+  const concentrixUser =
+    userData && userData.user && userData.user.concentrixUser
+      ? userData.user.concentrixUser
+      : false;
   useEffect(() => {
     dispatch(fetchBookings({ startDate: startDate, endDate: endDate }));
   }, [dispatch, startDate, endDate]);
 
-  return (
-    concentrixUser? <h1>You do not have access for this section</h1>:(
-      <div>
+  return concentrixUser ? (
+    <h1>You do not have access for this section</h1>
+  ) : (
+    <div>
       <h3>All Bookings</h3>
       <div className="container">
         <div>
@@ -109,7 +108,7 @@ const Booking = () => {
             'addressCompoundCode',
             'addressArea',
             'userId',
-            'appointmentAt'
+            'appointmentAt',
           ]}
           viewBookingButton={'view'}
           bookingDetails={handleBookingDetail}
@@ -118,7 +117,6 @@ const Booking = () => {
         <LoaderComponent />
       )}
     </div>
-    )
   );
 };
 
--- src/pages/Coupons/Coupons.jsx
+++ src/pages/Coupons/Coupons.jsx
@@ -1,23 +1,19 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteCoupon, fetchCoupon } from '../../store/actions/couponsAction';
-// import { fetchCoupon } from '../../store/actions/couponsAction';
 import TableComponent from '../../components/common/TableComponent/TableComponent';
 import LoaderComponent from '../../components/common/LoaderComponent/LoaderComponent';
 import moment from 'moment';
-import {
-  Button,
-} from '@mui/material';
+import { Button } from '@mui/material';
 import { useNavigate } from 'react-router';
 import ConfirmDialog from '../../components/common/Dialog/ConfirmDialog';
 
-
 const Coupons = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [openDialog, setOpenDialog] = useState(false);
-  const [deleteCouponData, setDeleteCouponData] = useState({})
+  const [deleteCouponData, setDeleteCouponData] = useState({});
   const couponList = useSelector((state) => state.coupons.couponList);
 
   useEffect(() => {
@@ -25,12 +21,22 @@ const Coupons = () => {
   }, [dispatch]);
 
   // Function to format start dates using moment
-  const formatDate = (date) => {
-    return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+  // const formatDate = (date) => {
+  //   return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+  // };
+  const formatDateTime = (dateTime) => {
+    const formattedDate = moment(dateTime).format('MMMM Do YYYY');
+    const formattedTime = moment(dateTime).format('h:mm:ss a');
+    return {
+      date: formattedDate,
+      time: formattedTime,
+    };
   };
 
   const modifyCouponList = (originalList) => {
     const formattedCouponList = originalList.map((coupon) => {
+      const formattedStart = formatDateTime(coupon.startDate);
+      const formattedExpiry = formatDateTime(coupon.expiryDate);
       const obj = {
         ID: coupon.id,
         'Coupon Code': coupon.code,
@@ -41,82 +47,87 @@ const Coupons = () => {
         'Minimum Cart Discount': coupon.minCartDiscount,
         'Maximum Cart Discount': coupon.maxCartDiscount,
         'Discount Type': coupon.discountType,
-        'Start Date': formatDate(coupon.startDate),
-        'Expiry Date': formatDate(coupon.expiryDate),
+        // 'Start Date': formatDate(coupon.startDate),
+        // 'Expiry Date': formatDate(coupon.expiryDate),
+        'Start Date': formattedStart.date,
+        'Start Time': formattedStart.time,
+        'Expiry Date': formattedExpiry.date,
+        'Expiry Time': formattedExpiry.time,
       };
       return obj;
     });
     return formattedCouponList;
   };
   const modifiedCouponList = modifyCouponList(couponList);
-
+  console.log(modifiedCouponList);
   const handleDeleteCoupon = async (couponData) => {
-    setDeleteCouponData(couponData)
+    setDeleteCouponData(couponData);
     setOpenDialog(true);
-  }
-
+  };
 
   const addCouponClickHandler = () => {
-    navigate('/add-coupon')
-  }
+    navigate('/add-coupon');
+  };
   const handleCloseDialog = () => {
-    setDeleteCouponData({})
+    setDeleteCouponData({});
     setOpenDialog(false);
   };
 
-  const handleConfirm = ()=>{
-    
-    const id = deleteCouponData.ID
-    deleteCoupon(id).then(response=> {
-      if (response?.status?.code === 200) {
-        setDeleteCouponData({})
-        window.location.reload()
-      } else {
-        alert("Can not delete coupon")
-      }
-    }).catch(err=>{
-      alert(err)
-    })
-    
-  }
+  const handleConfirm = () => {
+    const id = deleteCouponData.ID;
+    deleteCoupon(id)
+      .then((response) => {
+        if (response?.status?.code === 200) {
+          setDeleteCouponData({});
+          window.location.reload();
+        } else {
+          alert('Can not delete coupon');
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
-  const stringifiedUser = localStorage.getItem("userData")
-  const userData = stringifiedUser? JSON.parse(stringifiedUser):null
-  const hasCouponAccess = userData && userData.user && userData.user.hasCouponAccess? userData.user.hasCouponAccess:false 
+  const stringifiedUser = localStorage.getItem('userData');
+  const userData = stringifiedUser ? JSON.parse(stringifiedUser) : null;
+  const hasCouponAccess =
+    userData && userData.user && userData.user.hasCouponAccess
+      ? userData.user.hasCouponAccess
+      : false;
 
   return (
     <>
-    <ConfirmDialog isOpen = {openDialog}
-                    onClose={handleCloseDialog}
-                    onConfirm = {handleConfirm}
-                    />
-      {
-        hasCouponAccess? (
+      <ConfirmDialog
+        isOpen={openDialog}
+        onClose={handleCloseDialog}
+        onConfirm={handleConfirm}
+      />
+      {hasCouponAccess ? (
+        <div>
           <div>
-    
-          <div>
-    
             <h3>Coupons</h3>
             <>
-            
               <Button
                 concentrixUservariant="contained"
                 color="primary"
-                style={{ margin: '10px' ,backgroundColor:'#384456',
-                color: 'white',
-                transition: 'transform 0.3s,background-color 0.3s'
-            
-              }}
-              onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.1)')
-            }
-              onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
-            
+                style={{
+                  margin: '10px',
+                  backgroundColor: '#384456',
+                  color: 'white',
+                  transition: 'transform 0.3s,background-color 0.3s',
+                }}
+                onMouseOver={(e) =>
+                  (e.currentTarget.style.transform = 'scale(1.1)')
+                }
+                onMouseOut={(e) =>
+                  (e.currentTarget.style.transform = 'scale(1)')
+                }
                 onClick={addCouponClickHandler}
               >
                 Add Coupon
               </Button>
-            
-              
+
               {modifiedCouponList?.length > 0 ? (
                 <TableComponent
                   data={modifiedCouponList}
@@ -126,15 +137,13 @@ const Coupons = () => {
               ) : (
                 <LoaderComponent />
               )}
-    
             </>
           </div>
         </div>
-        ):<h1>You do not have access for coupons</h1>
-      }
+      ) : (
+        <h1>You do not have access for coupons</h1>
+      )}
     </>
-
-
   );
 };
 
