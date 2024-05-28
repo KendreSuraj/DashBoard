9026997f6fdd14dbaa647aba3f56270cf18f27a4
Praduman <admin@Admin.local>
2024-05-28 11:46:19 +0530

    fix booking api

--- src/store/actions/booking.action.js
+++ src/store/actions/booking.action.js
@@ -124,3 +124,23 @@ export const addBookingPayment = async (formData) => {
     throw error;
   }
 };
+
+export const fetchBookingsByPartner = async (params) => {
+  try {
+    let bookingApiUrl = `${apiUrl}/api/v1/admin/booking/bookings?startDate=${params.startDate}&endDate=${params.endDate}&page=${params.page}`;
+    if (params.searchType && params.searchText) {
+      bookingApiUrl += `&${params.searchType}=${params.searchText}`;
+    }
+    const res = await axios.get(bookingApiUrl, {
+      headers: {
+        Authorization: `Basic ${process.env.REACT_APP_ADMIN_APP_KEY}`,
+        token: getToken(),
+      },
+    });
+    const data = res.data;
+    return data;
+  } catch (error) {
+    console.error('Error in fetchBookingsByPartner:', error);
+    return null;
+  }
+};
