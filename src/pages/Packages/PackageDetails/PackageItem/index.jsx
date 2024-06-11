2fb26e314fffe7779ebb9aa07422c42bd2f19b64
varungupta2906 <varun.gupta@primathon.in>
2024-06-11 16:43:04 +0530

    Bugfix: Packages

--- src/pages/Packages/PackageDetails/PackageItem/index.jsx
+++ src/pages/Packages/PackageDetails/PackageItem/index.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { TextField, FormControl } from '@mui/material';
+import { TextField, FormControl, FormControlLabel, Checkbox } from '@mui/material';
 import axios from 'axios';
 import { getToken } from '../../../../components/common/userLocalStorageUtils';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const PackageItem = ({ index, rule, names, onChange, setPrice, price,onDelete }) => {
+const bodyparts = ["Hands", "Chest", "Legs", "Abdomen", "Head", "Face"];
+
+const PackageItem = ({ index, rule, names, onChange, setPrice, price, onDelete }) => {
     const apiUrl = process.env.REACT_APP_API_URL;
     const [productSession, setProductSession] = useState();
     const [previousSession, setPreviousSession] = useState();
     const packageType = localStorage.getItem('packageDetail');
+    const [addBodyParts, setAddBodyParts] = useState(false);
+    const [parts, setParts] = useState([]);
 
     const handleProductChange = async (event) => {
         if (event.target.innerHTML !== "") {
@@ -31,7 +35,7 @@ const PackageItem = ({ index, rule, names, onChange, setPrice, price,onDelete })
         } else if (sessions > productSession.length) {
             alert(`Sessions should be less than or equal to ${productSession.length}`);
         }
-        else if(sessions<0){
+        else if (sessions < 0) {
             alert("Cannot add negative values")
         }
         else if (sessions < 1) {
@@ -47,20 +51,31 @@ const PackageItem = ({ index, rule, names, onChange, setPrice, price,onDelete })
     };
     const productID = parseInt(rule.productId);
 
+    const handleCheckboxChange = (event) => {
+        setAddBodyParts(event.target.checked);
+    };
+
+    const handleBodyParts = (event, value) => {
+        setParts(value);
+    }
+
     return (
         <div style={{ display: "flex", gap: "20px" }}>
-            <FormControl style={{ width: "400px" }}>
-                <Autocomplete
-                    disablePortal
-                    id="combo-box-demo"
-                    options={names}
-                    value={names[productID - 1]}
-                    onChange={handleProductChange}
-                    sx={{ width: 400 }}
-                    disabled={packageType === "edit" && rule.sessions}
-                    renderInput={(params) => <TextField {...params} label="Products" />}
-                />
-            </FormControl>
+            <div>
+                <FormControl style={{ width: "400px" }}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={names}
+                        value={names[productID - 1]}
+                        onChange={handleProductChange}
+                        sx={{ width: 400 }}
+                        disabled={packageType === "edit" && rule.sessions}
+                        renderInput={(params) => <TextField {...params} label="Products" />}
+                    />
+                </FormControl>
+                {productSession && !rule.sessions && <p style={{ fontSize: "20px", paddingTop: "10px" }}>Max number of Sessions Allowed: {productSession?.length}</p>}
+            </div>
 
             <TextField
                 variant="outlined"
@@ -73,8 +88,29 @@ const PackageItem = ({ index, rule, names, onChange, setPrice, price,onDelete })
                 onChange={handleSessionsChange}
                 required
             />
-            {productSession  && !rule.sessions&& <p style={{ fontSize: "20px", paddingTop: "10px" }}>Max number of Sessions Allowed: {productSession?.length}</p>}
-            {packageType === "edit"&& rule.sessions && <button style={{
+            {!rule.sessions &&
+                <>
+                    <FormControlLabel
+                        control={<Checkbox checked={addBodyParts} onChange={handleCheckboxChange} />}
+                        label="Want to add body parts?"
+                    />
+
+                    {addBodyParts && (
+                        <FormControl sx={{ width: "330px" }}>
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-demo"
+                                options={bodyparts}
+                                value={parts}
+                                onChange={handleBodyParts}
+                                multiple
+                                renderInput={(params) => <TextField {...params} variant="outlined" label="Body Parts" />}
+                            />
+                        </FormControl>
+                    )}
+                </>
+            }
+            {packageType === "edit" && rule.sessions && <button style={{
                 backgroundColor: "rgb(215, 0, 64)",
                 borderColor: "rgb(215, 0, 64)",
                 color: "white",
@@ -82,7 +118,7 @@ const PackageItem = ({ index, rule, names, onChange, setPrice, price,onDelete })
                 width: "100px",
                 borderRadius: "10px"
             }}
-            onClick={onDelete}>DELETE</button>}
+                onClick={onDelete}>DELETE</button>}
         </div>
     );
 };
