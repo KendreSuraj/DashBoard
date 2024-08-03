e379c430049389b22a37ce51431693fb97c7018e
varungupta2906 <varun.gupta@primathon.in>
2024-08-03 14:30:50 +0530

    amount paid

--- src/components/common/BookingComponent/PaymentHistory.jsx
+++ src/components/common/BookingComponent/PaymentHistory.jsx
@@ -11,17 +11,18 @@ const PaymentHistory = ({ sessionScheduleId, orderId }) => {
     const dispatch = useDispatch();
     const paymentHistory = useSelector((state) => state?.booking?.paymentHistory)
     useEffect(() => {
-        dispatch(fetchPaymentHistory({sessionScheduleId,orderId}))
-    }, [sessionScheduleId,orderId]);
+        dispatch(fetchPaymentHistory({ sessionScheduleId, orderId }))
+    }, [sessionScheduleId, orderId]);
     const remainingAmount = paymentHistory.remainingAmount
     return (
         <div>
             <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
                 <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    {hasAdminAndSuperAdminAccess(role) && <Button onClick={() => navigate("/add-booking-payment", { state: { sessionScheduleId, remainingAmount ,orderId} })} style={{ color: '#FFFFFF', background: '#0C2294', borderRadius: '8px', textTransform: 'capitalize' }}>Add Payment</Button>}                </div> <h3>Payment History</h3>
+                    {hasAdminAndSuperAdminAccess(role) && <Button onClick={() => navigate("/add-booking-payment", { state: { sessionScheduleId, remainingAmount, orderId } })} style={{ color: '#FFFFFF', background: '#0C2294', borderRadius: '8px', textTransform: 'capitalize' }}>Add Payment</Button>}                </div> <h3>Payment History</h3>
                 <div style={{ display: 'flex' }}>
                     <h4 style={{ borderBottom: '1px solid black', marginRight: '50px', paddingBottom: '5px' }}>Total Amount: {paymentHistory.totalAmount}</h4>
-                    <h4 style={{ borderBottom: '1px solid black', paddingBottom: '5px' }}>Remaining Amount: {paymentHistory.remainingAmount}</h4>
+                    <h4 style={{ borderBottom: '1px solid black', paddingBottom: '5px', marginRight: '50px' }}>Amount Paid: {paymentHistory.totalAmount - paymentHistory.remainingAmount}</h4>
+                    <h4 style={{ borderBottom: '1px solid black', paddingBottom: '5px' }}>Amount Pending: {paymentHistory.remainingAmount}</h4>
                 </div>
                 <div className="table-container">
                     <table className="custom-table">
